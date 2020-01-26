d405163b935f5964ab8e008353cd9f9499b34f76
Grace Westerman <gracewes@tamu.edu>
2020-01-25 22:11:14 -0600

    generate list of tuples to check crashes per area

--- getDirections.py
+++ getDirections.py
@@ -1,6 +1,21 @@
 import requests, json
+secondsThreshold = 120
 APIKEY = "secret"
 url = "https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=" + APIKEY
-response = requests.get(url)
-r = json.loads(response.text)
-print(r["legs"])
+
+# Using API Request
+#response = requests.get(url)
+#r = json.loads(response.text)
+
+# Using Dummy Data
+with open("./exampleResponse.json") as json_file:
+    response = json.load(json_file)
+r = response
+
+steps = r["routes"][0]["legs"][0]["steps"]
+stepBounds = []
+for a in range(len(steps)):
+    if steps[a]["duration"]["value"] > secondsThreshold:
+        stepBounds.append([(steps[a]["start_location"]["lat"], steps[a]["start_location"]["lng"]) , (steps[a]["end_location"]["lat"], steps[a]["end_location"]["lng"])])
+
+print (stepBounds)
